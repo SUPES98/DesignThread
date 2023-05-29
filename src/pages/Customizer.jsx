40751863d177c0844902250027c6bd7fb62eca7c
Bigbaby Yoda <supratim1998@gmail.com>
2023-05-29 20:00:33 +0530

    first commit

---
+++ src/pages/Customizer.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import config from '../config/config'
+
+const Customizer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Customizer
