29fde6643606b482755b444e660cf76ffdb2b6f1
Bigbaby Yoda <supratim1998@gmail.com>
2023-05-30 11:13:31 +0530

    Added canvas and components

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import state from '../store'
-import { useSnapshot } from 'valtio'
+import state from "../store";
+import { useSnapshot } from "valtio";
 
-const CustomButton = ({type, title, customStyles, handleClick}) => {
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
   const snap = useSnapshot(state);
   const generateStyle = (type) => {
-    if(type=== "filled"){
-      return{
+    if (type === "filled") {
+      return {
         backgroundColor: snap.color,
-        color: '#fff'
-      }
+        color: "#fff",
+      };
     }
-  }
+  };
   return (
     <button
-    className={`px-2 py-1.2 flex-1 rounded-md ${customStyles}`}
-    style={generateStyle(type)}
-    onClick={handleClick}
+      className={`px-2 py-1.2 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
     >
       {title}
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
